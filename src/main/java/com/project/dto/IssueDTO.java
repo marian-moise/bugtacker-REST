ea18d698a7640fddddc824dbfe7c260ac1462ae2
marian_moise <marian_moise@ymail.com>
2020-08-31 22:06:45 +0300

    Added constructor with projectId and name

--- src/main/java/com/project/dto/IssueDTO.java
+++ src/main/java/com/project/dto/IssueDTO.java
@@ -9,6 +9,11 @@ public class IssueDTO implements Serializable {
     String identifier;
     String description;
 
+    public IssueDTO(Integer projectId, String name) {
+        this.projectId = projectId;
+        this.name = name;
+    }
+
     public IssueDTO() {
     }
 
